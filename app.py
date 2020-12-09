f81e2277780d32b9db62d2215a912bc26ee204ba
pablomadridm <68798975+pablomadridm@users.noreply.github.com>
2020-12-08 21:36:27 -0500

    Segundo cambio

--- app.py
+++ app.py
@@ -5,18 +5,36 @@ app = Flask(__name__)
 
 @app.route('/login')
 def login():
-    return render_template("login.html")
-
+    return render_template('login.html')
 
 @app.route('/main-menu')
 def menu():
     return render_template('main-menu.html')
 
 
+@app.route('/crear-usuario')
+def crear_usuario():
+    return render_template('CrearUsuario.html')
+
+
 @app.route('/mod-usuario')
 def mod_usuario():
     return render_template('modificarUsuario.html')
 
+
+@app.route('/lista')
+def lista():
+    return render_template('lista-productos.html')
+
+
+@app.route('/buscar-producto')
+def buscar_producto():
+    return render_template('busca-producto.html')
+
+
+@app.route('/producto')
+def producto():
+    return render_template('pagina-unitaria-producto.html')
 
 
 if __name__ == '__main__':
