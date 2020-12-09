6a725abcf03f0876cdf1e373947578c8111294f4
pablomadridm <68798975+pablomadridm@users.noreply.github.com>
2020-12-08 22:25:40 -0500

    Creación link de home

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+app.secret_key = "mischiefManaged"
+
+@app.route('/')
+def home():
+    return render_template('login.html')
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template('login.html')
+
 
 @app.route('/main-menu')
 def menu():
